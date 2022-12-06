a1a645cb536ef5a8056dc96a3a912eec725cf0a7
Romecodes <olisaokoyej@gmail.com>
2022-12-06 02:41:15 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-for num in my_list:
-print('{:d}'.format(num))
+
+def element_at(my_list, idx):
+if idx < 0:
+return None
+elif idx > (len(my_list) - 1):
+return None
+
+else:
+return my_list[idx]
